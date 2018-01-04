7fba54227fdc6f628f3bb3fcfe7038f4e392c30f
Steven Chadwick <schad@uw.edu>
2018-01-04 15:16:40 -0800

    Add multiple netids in one call #4

--- test/unit/membership-test.js
+++ test/unit/membership-test.js
@@ -1,8 +1,9 @@
+/* eslint-disable max-nested-callbacks */
 require('../setup');
 
 let group = 'uw_foster_staff_it_developers_nodegws-test_group';
 
-describe('Membership', function () {
+describe('Membership', () => {
 
   beforeEach(() => {
     uwgws.initialize(config);
@@ -25,6 +26,16 @@ describe('Membership', function () {
       expect(result.statusCode).to.equal(200);
     });
 
+    it('should add multiple members to a group', async () => {
+      let options = {
+        id:    group,
+        netid: ['dgale', 'gabugabu']
+      };
+
+      let result = await uwgws.membership.add(options);
+      expect(result.statusCode).to.eql(200);
+    });
+
     it('should add a rollup group', async () => {
       let options = {
         id:    group,
@@ -33,26 +44,6 @@ describe('Membership', function () {
 
       let result = await uwgws.membership.add(options);
       expect(result.statusCode).to.eql(200);
-    });
-  });
-
-  describe('Get', () => {
-    it('should return membership', async () => {
-      let options = {id: group};
-
-      // this test occasionally returns 404
-      let result = await uwgws.membership.get(options);
-      expect(result.data.length).to.be.within(1, 4);
-    });
-
-    it('should return effective membership', async () => {
-      let options = {
-        effective: true,
-        id:        group
-      };
-
-      let result = await uwgws.membership.get(options);
-      expect(result.data.length).to.be.within(10, 20);
     });
   });
 
@@ -75,6 +66,27 @@ describe('Membership', function () {
       let result = await uwgws.membership.del(options);
       expect(result.statusCode).to.equal(400);
     });
+  });
 
+  describe('Get', () => {
+    before(done => setTimeout(() => done(), 6000));
+
+    it('should return membership', async () => {
+      let options = {id: group};
+
+      // this test occasionally returns 404
+      let result = await uwgws.membership.get(options);
+      expect(result.data.length).to.be.within(1, 4);
+    });
+
+    it('should return effective membership', async () => {
+      let options = {
+        effective: true,
+        id:        group
+      };
+
+      let result = await uwgws.membership.get(options);
+      expect(result.data.length).to.be.within(10, 20);
+    });
   });
 });

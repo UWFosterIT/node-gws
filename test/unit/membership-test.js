2f86a78f9ae8816d7fcf91509b4b9d79c4c12fa9
dgale <dgale@uw.edu>
2018-01-09 19:22:46 -0800

    add error and message fields to return values. expand testing to cover business logic errors. #5

--- test/unit/membership-test.js
+++ test/unit/membership-test.js
@@ -2,6 +2,8 @@
 require('../setup');
 
 let group = 'uw_foster_staff_it_developers_nodegws-test_group';
+let nonGroup = 'fake_group_that_is_not_real_at_all';
+let unauthorizedGroup = 'uw_employee';
 
 describe('Membership', () => {
 
@@ -24,6 +26,8 @@ describe('Membership', () => {
 
       let result = await uwgws.membership.add(options);
       expect(result.statusCode).to.equal(200);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
     });
 
     it('should add multiple members to a group', async () => {
@@ -34,6 +38,8 @@ describe('Membership', () => {
 
       let result = await uwgws.membership.add(options);
       expect(result.statusCode).to.eql(200);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
     });
 
     it('should add a rollup group', async () => {
@@ -44,7 +50,83 @@ describe('Membership', () => {
 
       let result = await uwgws.membership.add(options);
       expect(result.statusCode).to.eql(200);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
     });
+
+    // Catching add errors
+    it('should return an error if the netid is invalid', async () => {
+      let options = {
+        id:    group,
+        netid: 'dgalefakenetid'
+      };
+
+      let result = await uwgws.membership.add(options);
+      expect(result.statusCode).to.equal(200);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(1);
+    });
+
+    it('should return an error if the group is invalid', async () => {
+      let options = {
+        id:    nonGroup,
+        netid: 'dgale'
+      };
+
+      let result = await uwgws.membership.add(options);
+      expect(result.statusCode).to.equal(404);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(1);
+    });
+
+    it('should return an error if any net ids are invalid', async () => {
+      let options = {
+        id:    group,
+        netid: ['dgalefakenetid', 'gabugabufakenetid', 'schad']
+      };
+
+      let result = await uwgws.membership.add(options);
+      expect(result.statusCode).to.eql(200);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(2);
+    });
+
+    it('does NOT throw an error when adding an existing member', async () => {
+      let options = {
+        id:    group,
+        netid: 'milesm'
+      };
+
+      let result = await uwgws.membership.add(options);
+      expect(result.statusCode).to.equal(200);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
+    });
+
+    it('should return an error when adding an invalid rollup group', async () => {
+      let options = {
+        id:    group,
+        netid: nonGroup
+      };
+
+      let result = await uwgws.membership.add(options);
+      expect(result.statusCode).to.eql(200);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(1);
+    });
+
+    it('should return an error when adding a member to a group not authorized to administer', async () => {
+      let options = {
+        id:    unauthorizedGroup,
+        netid: 'dgale'
+      };
+
+      let result = await uwgws.membership.add(options);
+      expect(result.statusCode).to.equal(401);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(1);
+    });
+
   });
 
   describe('Delete', () => {
@@ -54,10 +136,58 @@ describe('Membership', () => {
         netid: 'schad'
       };
 
-      let result = await uwgws.membership.add(options);
-      expect(result.statusCode).to.equal(200);
       result = await uwgws.membership.del(options);
       expect(result.statusCode).to.equal(200);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
+    });
+
+    it('does NOT throw an error when deleting a non-member (valid netid) from the group', async () => {
+      let options = {
+        id:    group,
+        netid: 'schad'
+      };
+
+      result = await uwgws.membership.del(options);
+      expect(result.statusCode).to.equal(200);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
+    });
+
+    it('does NOT throw an error when attemping to delete an invalid netid from the group', async () => {
+      let options = {
+        id:    group,
+        netid: 'schadfakenetidn'
+      };
+
+      result = await uwgws.membership.del(options);
+      expect(result.statusCode).to.equal(200);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
+    });
+
+    it('throws an error when attemping to delete from an invalid group', async () => {
+      let options = {
+        id:    nonGroup,
+        netid: 'dgale'
+      };
+
+      result = await uwgws.membership.del(options);
+      expect(result.statusCode).to.equal(404);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(1);
+    });
+
+    it('throws an error when attemping to delete from a group we are not authorized to administer', async () => {
+      let options = {
+        id:    unauthorizedGroup,
+        netid: 'dgale'
+      };
+
+      result = await uwgws.membership.del(options);
+      expect(result.statusCode).to.equal(401);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(1);
     });
 
     it('should not delete all members from the group', async () => {
@@ -65,7 +195,10 @@ describe('Membership', () => {
 
       let result = await uwgws.membership.del(options);
       expect(result.statusCode).to.equal(400);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(1);
     });
+
   });
 
   describe('Get', () => {
@@ -76,7 +209,10 @@ describe('Membership', () => {
 
       // this test occasionally returns 404
       let result = await uwgws.membership.get(options);
-      expect(result.data.length).to.be.within(1, 4);
+      expect(result.statusCode).to.eql(200);
+      expect(result.data.length).to.be.within(1, 5);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
     });
 
     it('should return effective membership', async () => {
@@ -87,6 +223,8 @@ describe('Membership', () => {
 
       let result = await uwgws.membership.get(options);
       expect(result.data.length).to.be.within(10, 20);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
     });
 
     it('should return the membership history for a group', async () => {
@@ -94,8 +232,31 @@ describe('Membership', () => {
       let result = await uwgws.group.history(query);
 
       expect(result.statusCode).to.eql(200);
+      expect(result.error).to.equal(false);
+      expect(result.message.length).to.equal(0);
       expect(Array.isArray(result.data)).to.eql(true);
       expect(result.data.length).to.eql(6);
+    });
+
+    // Error handling
+    it('from an invalid group returns an error', async () => {
+      let options = {id: nonGroup};
+
+      let result = await uwgws.membership.get(options);
+      expect(result.statusCode).to.eql(404);
+      expect(result.data.length).to.equal(0);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(1);
+    });
+
+
+    it('history for an invalid group returns an error', async () => {
+      let query  = {id: nonGroup};
+      let result = await uwgws.group.history(query);
+      expect(result.statusCode).to.eql(404);
+      expect(result.error).to.equal(true);
+      expect(result.message.length).to.equal(1);
+      expect(result.data.length).to.equal(0);
     });
 
   });

e9b1424b03e6a8b90cd4f6fcb81ba18445912b63
Steven Chadwick <sschadwick@users.noreply.github.com>
2018-02-09 09:57:04 -0800

    Adding group.move functionality (#10)

--- test/unit/group-test.js
+++ test/unit/group-test.js
@@ -53,6 +53,26 @@ describe('Group', () => {
     expect(result.message.length).to.eql(0);
   });
 
+  it('should move a group to a new stem', async () => {
+    options.name = 'uw_foster_student_move';
+    await uwgws.group.create(options);
+
+    let query = {
+      id:      'uw_foster_student_move',
+      newStem: 'uw_foster_staff'
+    };
+    let result = await uwgws.group.move(query);
+    expect(result.statusCode).to.eql(200);
+    expect(result.error).to.eql(false);
+
+    let getRes = await uwgws.group.get({id: 'uw_foster_staff_move'});
+    expect(getRes.error).to.eql(false);
+    expect(getRes.data.contact).to.eql('milesm');
+    expect(getRes.data.title).to.eql('Foster GWS Test');
+
+    await uwgws.group.del({id: 'uw_foster_staff_move'});
+  });
+
   // Error handling
   it('should return an error when getting an invalid group', async () => {
     let query  = {id: nonGroup};
@@ -62,15 +82,15 @@ describe('Group', () => {
     expect(result.message.length).to.eql(1);
   });
 
-  it('does not return an error when trying to delete an invalid group', async () => {
-    result = await uwgws.group.del({id: nonGroup});
-    expect(result.statusCode).to.eql(200);
-    expect(result.error).to.eql(false);
-    expect(result.message.length).to.eql(0);
+  it('should return an error when trying to delete an invalid group', async () => {
+    let result = await uwgws.group.del({id: nonGroup});
+    expect(result.statusCode).to.eql(404);
+    expect(result.error).to.eql(true);
+    expect(result.message.length).to.eql(1);
   });
 
   it('should return an error when trying to delete you are not authorized to administer', async () => {
-    result = await uwgws.group.del({id: unauthorizedGroup});
+    let result = await uwgws.group.del({id: unauthorizedGroup});
     expect(result.statusCode).to.eql(401);
     expect(result.error).to.eql(true);
     expect(result.message.length).to.eql(1);

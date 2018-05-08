9acf4f311fb1bc8ba73c07534f8d2f3ff9ed2f49
Kevin Coleman <kevin.github@kevintechie.com>
2018-05-08 12:04:19 -0700

    updating config

--- test/setup/config-sample.js
+++ test/setup/config-sample.js
@@ -2,8 +2,20 @@ module.exports = {
   baseUrl:   'https://URL/to/Web/Service/',
   cacheExt:  '.json',
   cacheMode: 'wild',
-  cachePath: '/FULL/PATH/TO/where you want to cache http requests/',
-  cert:      '/FULL/PATH/TO/509Cert.cer',
-  key:       '/FULL/PATH/TO/509Key.key',
-  logLevel:  'info'
+  cachePath: '.cache/',
+  certInfo:  {
+    file: {
+      cert: 'PATH TO LOCAL CERT',
+      key:  'PATH TO LOCAL KEY'
+    },
+    // s3: {
+    //   certBucket: 'CERT AWS BUCKET',
+    //   certKey:    'CERT AWS KEY',
+    //   keyBucket:  'KEY AWS BUCKET',
+    //   keyKey:     'KEY AWS KEY'
+    // }
+  },
+  logLevel: 'info',
 };
+
+module.exports = config;
